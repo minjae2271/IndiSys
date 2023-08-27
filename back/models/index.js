982c3266058464184cc4348ada251b549b0b28b6
minjae <minjae2271@naver.com>
2023-08-27 16:57:19 +0900

    prod console

--- back/models/index.js
+++ back/models/index.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.js')[env];
 const db = {};
-
+console.log(env);
+console.log(env.length);
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.User = require('./user')(sequelize, Sequelize);
